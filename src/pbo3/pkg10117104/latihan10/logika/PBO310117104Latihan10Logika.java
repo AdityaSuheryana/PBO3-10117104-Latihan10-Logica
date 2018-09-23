726586984fcd8ccdbe01e624ed3e130fd919f733
Aditya Suheryana <adityasuheryanaa@gmail.com>
2018-09-23 18:38:53 +0700

    first commit

---
+++ src/pbo3/pkg10117104/latihan10/logika/PBO310117104Latihan10Logika.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117104.latihan10.logika;
+
+/**
+ *
+ * @author  
+ * Nama      : Aditya Suheryana
+ * Kelas     : PBO-3
+ * Nim       : 10117104
+ * Deskripsi : Program ini berisi Logika Boolean 
+ */
+public class PBO310117104Latihan10Logika {
+
+    public static void main(String[] args) {
+       int umur =19;
+       boolean dewasa;
+       dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+               
+    }
+    
+}
